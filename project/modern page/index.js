db8c036be90ca03a6258f577ab9ea86d19a60275
Aarya <saarya903@gmail.com>
2021-04-29 00:44:10 +0530

    make more responsive layout

--- project/modern page/index.js
+++ project/modern page/index.js
@@ -1,8 +1,4 @@
     let toggle = document.querySelector('.toggle');
-    let anchor = document.querySelector('.nav-link');
     toggle.onclick = () => {
         toggle.classList.toggle('active');
-    }
-    anchor.onclick = () => {
-        anchor.classList.toggle('active');
     }
